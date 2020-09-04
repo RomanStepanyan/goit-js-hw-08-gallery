3e133137153d1334c3b9aadbfad67a4af81c4c5e
RomanStepanyan <stepik10@gmail.com>
2020-09-04 17:53:38 +0300

    Gallery and modal window are ready to use

--- task-1.js
+++ task-1.js
@@ -1,6 +1,10 @@
 import images from './gallery-items.js'
 
-const galleryUlRef = document.querySelector('.js-gallery')
+const modalWindowRef = document.querySelector('.js-lightbox');
+const galleryUlRef = document.querySelector('.js-gallery');
+const imglightboxRef = document.querySelector('.lightbox__image');
+const btnCloseModalRef = document.querySelector('.lightbox__button');
+const lightboxContainer = document.querySelector('.lightbox__content');
 const creatingLiTag = () =>{
     const li = document.createElement('li');
     return li;
@@ -16,16 +20,30 @@ const creatingImgTag = () => {
 
 images.forEach(image => {
     const liTags = creatingLiTag(image);
-    liTags.class = 'gallery__item';
+    liTags.setAttribute('class', 'gallery__item');
     const aTags = creatingATag(image);
-    aTags.class = 'gallery__link';
-    aTags.href = image.original;
+    aTags.setAttribute('class', 'gallery__link');
+    aTags.setAttribute('href', image.original);
     const imgTags = creatingImgTag(image);
-    imgTags.class = 'gallery__image';
-    imgTags.src = image.preview;
-    imgTags.alt = image.description;
+    imgTags.setAttribute('class', 'gallery__image');
+    imgTags.setAttribute('src', image.preview);
+    imgTags.setAttribute('alt', image.description);
     imgTags.setAttribute('data-source', image.original);
     aTags.appendChild(imgTags);
     liTags.appendChild(aTags);    
     galleryUlRef.appendChild(liTags)
+})
+galleryUlRef.addEventListener('click', event => {
+    event.preventDefault();
+    modalWindowRef.classList.add('is-open');
+    imglightboxRef.src = event.target.dataset.source;
+})
+
+modalWindowRef.addEventListener('click', event =>{
+    if (event.target === btnCloseModalRef || lightboxContainer){        
+        console.log('event.target', event.target);
+        console.log('event.currentTarget', event.currentTarget)
+        modalWindowRef.classList.remove('is-open');
+        imglightboxRef.src = '';
+    } 
 })
